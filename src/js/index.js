b88b9409859a8000d1d3f37cce9a958f0705cf35
Kubihub <kuburatuadiza5@gmail.com>
2023-10-23 15:35:18 +0000

    first commit for the tailwind css project

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+// Initialization for ES Users
+import {
+    Collapse,
+    Dropdown,
+    initTE,
+  } from "tw-elements";
+  
+  initTE({ Collapse, Dropdown,Ripple  });
+
+  // Initialization for ES Users
+import {
+  Input,
+  Ripple,
+  initTE,
+} from "tw-elements";
+
+initTE({ Input, Ripple });
